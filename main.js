5a8205776497a705dbca6464abff6fe9a0bf7ca5
Anita.Chou <Anita.Chou@avermedia.com>
2024-03-10 18:46:37 +0800

    Add image zoom out

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 $(function(){
+	let $mail = $('.letter-image');
 	let $letter = $('.letter');
 	let $page = $('.page');
+	let $imgDetail = $('.img_detail');
 	let letterWidth = $letter.width();
 
-	$letter.click(function() {
-		$letter.width(letterWidth);
+	$mail.on( "mouseenter", function() {
+		$(this).addClass('active');
+	});
+
+	$letter.on( "mouseenter", function() {
+		$letter.css('overflow','visible');
 		$page.addClass('active');
+	});
+
+	$letter.click(function() {
+		$imgDetail.css({
+			'z-index' : 99,
+			'display' : 'flex',
+			'position' : 'fixed'
+		});
 	});
 });
